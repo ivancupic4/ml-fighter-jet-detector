56f8a8f785b1de94b070b190d9083f2dd730a03f
Ivan Cupic <Ivan.Cupic@bullhorn.com>
2024-06-05 15:46:04 +0200

    Add project files.

---
+++ MLFighterJetDetector_WebApi1/FinalResult.cs
@@ -0,0 +1,8 @@
+﻿namespace MLFighterJetDetector_WebApi1
+{
+    public class FinalResult
+    {
+        public IOrderedEnumerable<KeyValuePair<string, float>> Scores { get; set; }
+        public double TimeInMs { get; set; }
+    }
+}
